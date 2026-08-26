297c568aad1771784fb30bc9f22deaa4af0d63f9
zhufucong <zhufucong_zfc@163.com>
2026-08-27 00:00:16 +0800

    feat: 测试detekt，自定义data class规则；

---
+++ recruit/src/main/java/com/example/recruit/data/RecruitmentBizModel.kt
@@ -0,0 +1,9 @@
+package com.example.recruit.data
+
+import androidx.annotation.Keep
+
+/**
+ * 招工信息
+ */
+@Keep
+data class RecruitmentBizModel(val title: String?, val id: String?, val type: String?)
